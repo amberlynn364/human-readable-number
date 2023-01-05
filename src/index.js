79baeb2e5875b6a46d67b50ad49f7e4234b6290e
amberlynn <amberlynn364@gmail.com>
2023-01-05 04:59:00 +0300

    feat: new function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function toReadable (number) {
+    if (number == 0) {
+        return 'zero';
+    } else {
+        return readablehundreds(number).trim()
+    }
   
+}
+
+let numerals = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+let tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+
+function readablehundreds (number) {
+    if (number > 99) {
+        return numerals[Math.floor(number / 100)] + ' hundred ' + readableTens(number % 100);
+    } else {
+        return readableTens (number);  
+    }
+}
+function readableTens (number) {
+    if (number < 20) {
+        return numerals[number];
+    } else {
+        return tens[Math.floor(number / 10)] + ' ' + numerals[number % 10];
+    }
 }
